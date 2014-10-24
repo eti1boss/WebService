8124c3e6ff4aa04f6c805eb009896ddbcdedee7b
eti1boss <e.bossuet@gmail.com>
2014-10-24 12:50:17 +0200

    Mise en place de l'upload de fichier [OK] Quelques modifs sur dropzone : ajout d'options

--- src/main/java/controllers/HelloServlet.java
+++ src/main/java/controllers/HelloServlet.java
@@ -1,20 +1,66 @@
 package controllers;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.File;
 import java.io.IOException;
 
 /**
  * Created by Bob on 23/10/2014.
  */
-@WebServlet(name = "hello", urlPatterns = "/prout")
+@WebServlet(name = "hello", urlPatterns = "/up")
+@MultipartConfig(fileSizeThreshold=1024*1024*10,    // 10 MB
+        maxFileSize=1024*1024*50,          // 50 MB
+        maxRequestSize=1024*1024*100)      // 100 MB
 public class HelloServlet extends HttpServlet {
+
+    private static final String UPLOAD_DIR = "uploads";
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        resp.getWriter().write("Hello Noob");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        // gets absolute path of the web application
+        String applicationPath = req.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String uploadFilePath = applicationPath + File.separator + UPLOAD_DIR;
+
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(uploadFilePath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdirs();
+        }
+        System.out.println("Upload File Directory="+fileSaveDir.getPath());
+
+        String fileName = null;
+        //Get all the parts from request and write it to the file on server
+        for (Part part : req.getParts()) {
+            fileName = getFileName(part);
+            part.write(fileName);
+        }
+
+        resp.getWriter().write(fileName + " successfully uploaded!");
     }
+
+    /**
+     * Utility method to get file name from HTTP header content-disposition
+     */
+    private String getFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        System.out.println("content-disposition header= "+contentDisp);
+        String[] tokens = contentDisp.split(";");
+        for (String token : tokens) {
+            if (token.trim().startsWith("filename")) {
+                return token.substring(token.indexOf("=") + 2, token.length()-1);
+            }
+        }
+        return "";
+    }
+
+
+
 }
--- src/main/java/controllers/UserController.java
+++ src/main/java/controllers/UserController.java
@@ -7,13 +7,23 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import java.io.IOException;
+
 /**
  * Created by Bob on 23/10/2014.
  */
 @Path("/users")
+@MultipartConfig(location="/")
 public class UserController {
     @Inject
     private UserService userService;
@@ -21,7 +31,7 @@ public class UserController {
     @Path("/all")
     @GET
     @Produces("application/json")
-    public JsonArray getAll(){
+    public JsonArray getAll() {
         JsonArrayBuilder builder = Json.createArrayBuilder();
 
         for (User user : userService.getAll()) {
@@ -30,10 +40,29 @@ public class UserController {
         return builder.build();
     }
 
-    @Path("/test")
-    @GET
+    @Path("/file")
+    @POST
     @Produces("application/json")
-    public String allez(){
-        return "NOM DE DIEU DE PUTAIN DE BORDEL DE MERDE DE CONNARD DE SALOPERIE D'ENCULE DE TA MERE";
+    public String doUpload(@Context HttpServletRequest request) throws IOException, ServletException {
+        JsonArrayBuilder array = Json.createArrayBuilder();
+
+        for (Part part : request.getParts()) {
+            String name = null;
+            long size = 0;
+            try {
+                name = part.getSubmittedFileName();
+                size = part.getSize();
+
+//                array.add(addFile(name, size, "anId"));
+                part.delete();
+
+            } catch (Exception e) {
+            }
+        }
+
+        JsonObject ret = Json.createObjectBuilder().add("files", array).build();
+        return "toto";
+
     }
+
 }
